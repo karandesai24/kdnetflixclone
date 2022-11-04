81f54866ffee36f703a6cdae8f042fe893aab5e1
karandesai24 <karan181716@gmail.com>
2022-11-04 19:27:16 +0530

    idxx

--- client/src/authContext/apiCalls.js
+++ client/src/authContext/apiCalls.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { loginFailure, loginStart, loginSuccess, logout } from "./AuthActions";
 
+import { axiosInstance } from "../config";
 export const login = async (user, dispatch) => {
   dispatch(loginStart());
   try {
-    const res = await axios.post("auth/login", user);
+    const res = await axiosInstance.post("auth/login", user);
     dispatch(loginSuccess(res.data));
   } catch (err) {
     dispatch(loginFailure());
