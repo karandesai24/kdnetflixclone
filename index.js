fecf10cfc7e330aaba330307055b7d36c3304cd2
karandesai24 <karan181716@gmail.com>
2022-12-03 15:22:00 +0530

    commit

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const cors = require("cors");
 };*/
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin:
+      "http://localhost:3000" || "https://enchanting-tuna-costume.cyclic.app",
   })
 );
 const mongoose = require("mongoose");
